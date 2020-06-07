925842d8c299cdfb3efa304e78e4c49f2dcc9d21
azhx <zhual@utschools.ca>
2020-06-06 23:02:28 -0400

    'pack4'

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+web: gunicorn application:app
 web: python app.py
--- app.py
+++ app.py
@@ -28,4 +28,5 @@ def getgraph():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 8080)), debug=True)
+    app.run()
+# host='0.0.0.0', port=int(os.getenv('PORT', 8080)), debug=True
