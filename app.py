2a07bc12f330fa07c93333f27e788b14a59e643a
azhx <zhual@utschools.ca>
2020-06-06 23:12:11 -0400

    'blaeurba'

--- app.py
+++ app.py
@@ -28,5 +28,5 @@ def getgraph():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 8080)), debug=True)
 # host='0.0.0.0', port=int(os.getenv('PORT', 8080)), debug=True
